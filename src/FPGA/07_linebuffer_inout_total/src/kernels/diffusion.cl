8b9c892e9eceda97054f2892023daa2541531cab
Stephen Roberts <stephenianroberts@gmail.com>
2017-04-04 17:22:37 +0100

    added multi thingy

---
+++ src/FPGA/07_linebuffer_inout_total/src/kernels/diffusion.cl
@@ -0,0 +1,75 @@
+// vim: set filetype=c:
+
+#include "common.h"
+
+__kernel void __attribute__ ((reqd_work_group_size(1, 1, 1)))
+krnl_temperature(__global float *restrict field,
+                 __global float *restrict registers) {
+  float cumsum = 0;
+  for (unsigned k = 1; k < KMAX-1; ++k) {
+    for (unsigned j = 1; j < JMAX-1; ++j) {
+      for (unsigned i = 1; i < IMAX-1; ++i) {
+        cumsum += field[k * JMAX * IMAX + j * IMAX + i];
+      }
+    }
+  }
+  registers[TEMPERATURE] = cumsum;
+}
+
+__kernel void __attribute__ ((reqd_work_group_size(1, 1, 1)))
+krnl_diffuse(__global float *restrict field_a,
+             __global float *restrict field_b,
+             float cx,
+             float cy,
+             float cz) {
+  float cumsum = 0;
+
+  local float linebuf_jprev[IMAX];
+  local float linebuf_jnext[IMAX];
+  local float linebuf_kprev[IMAX];
+  local float linebuf_knext[IMAX];
+  local float linebuf_in[IMAX];
+  local float linebuf_out[IMAX];
+  const size_t istride = 1;
+  const size_t jstride = IMAX;
+  const size_t kstride = IMAX*JMAX;
+  for (unsigned k = 1; k < KMAX-1; ++k) {
+    for (unsigned j = 1; j < JMAX-1; ++j) {
+      const size_t line_start = k * JMAX * IMAX + j * IMAX;
+      async_work_group_copy(linebuf_in, field_a + line_start, IMAX, 0);
+      async_work_group_copy(linebuf_jprev, field_a + line_start - jstride, IMAX, 0);
+      async_work_group_copy(linebuf_jnext, field_a + line_start + jstride, IMAX, 0);
+      async_work_group_copy(linebuf_kprev, field_a + line_start - kstride, IMAX, 0);
+      async_work_group_copy(linebuf_knext, field_a + line_start + kstride, IMAX, 0);
+      barrier(CLK_LOCAL_MEM_FENCE);
+      __attribute__((xcl_pipeline_loop))
+      for (unsigned i = 1; i < IMAX-1; ++i) {
+        const size_t center = line_start + i;
+        float result = linebuf_in[i]
+                     + cx * (linebuf_in[i-istride] - 2*linebuf_in[i] + linebuf_in[i+istride])
+                     + cy * (linebuf_jprev[i] - 2*linebuf_in[i] + linebuf_jnext[i])
+                     + cz * (linebuf_kprev[i] - 2*linebuf_in[i] + linebuf_knext[i]);
+        linebuf_out[i] = result;
+        // Reflective Boundary Condition
+        if (i==1) {
+          linebuf_out[i-istride] = result;
+        } else if (i == IMAX - 2) {
+          linebuf_out[i+istride] = result;
+        } 
+      }
+      async_work_group_copy(field_b + line_start, linebuf_out, IMAX, 0);
+      // TODO pretty sure this could be done far better
+      if (j == 1) {
+         async_work_group_copy(field_b + line_start - jstride, linebuf_out, IMAX, 0);
+      } else if (j == JMAX - 2) {
+         async_work_group_copy(field_b + line_start + jstride, linebuf_out, IMAX, 0);
+      } 
+      if (k == 1) {
+         async_work_group_copy(field_b + line_start - kstride, linebuf_out, IMAX, 0);
+      } else if (k == KMAX - 2) {
+         async_work_group_copy(field_b + line_start + kstride, linebuf_out, IMAX, 0);
+      } 
+      barrier(CLK_LOCAL_MEM_FENCE);
+    }
+  }
+}
---
+++ src/FPGA/07_linebuffer_inout_total/src/xcl_world.c
@@ -0,0 +1,177 @@
+#include "xcl_world.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <CL/cl.h>
+
+void XCLSetup(char *vendor, char *device, char *binary, struct XCLWorld* world) {
+  PopulatePlatform(vendor, world);
+  if (world->status != CL_SUCCESS) {
+    fprintf(stderr, "Failed to populate vendor platform: %s\n", vendor);
+    exit(EXIT_FAILURE);
+  }
+  PopulateDevice(device, world);
+  if (world->status != CL_SUCCESS) {
+    fprintf(stderr, "Failed to populate device: %s\n", device);
+    exit(EXIT_FAILURE);
+  }
+  PopulateContext(world);
+  if (world->status != CL_SUCCESS) {
+    fprintf(stderr, "Failed to populate context\n");
+    exit(EXIT_FAILURE);
+  }
+  PopulateQueue(world);
+  if (world->status != CL_SUCCESS) {
+    fprintf(stderr, "Failed to populate queue\n");
+    exit(EXIT_FAILURE);
+  }
+  PopulateProgram(binary, world);
+  if (world->status != CL_SUCCESS) {
+    fprintf(stderr, "Failed to populate binary %s: %d\n", binary, world->status);
+    exit(EXIT_FAILURE);
+  }
+}
+
+void XCLTeardown(struct XCLWorld* world) {
+  if (world) {
+    if (world->vendor_name) {
+      free(world->vendor_name);
+      world->vendor_name = NULL;
+    }
+    if (world->device_name) {
+      free(world->device_name);
+      world->device_name = NULL;
+    }
+    if (world->binary_name) {
+      free(world->binary_name);
+      world->binary_name = NULL;
+    }
+    if (world->binary_data) {
+      free(world->binary_data);
+      world->binary_data = NULL;
+    }
+    if (world->program) {
+      clReleaseProgram(world->program);
+      world->program = 0;
+    }
+    if (world->queue) {
+      clReleaseCommandQueue(world->queue);
+      world->queue = 0;
+    }
+    if (world->context) {
+      clReleaseContext(world->context);
+      world->context = 0;
+    }
+    if (world->device) {
+      clReleaseDevice(world->device);
+      world->device = 0;
+    }
+  }
+}
+
+void PopulatePlatform(char *vendor, struct XCLWorld* world) {
+  cl_uint num_platforms;
+  world->platform = 0;
+  world->status = clGetPlatformIDs(0, NULL, &num_platforms);
+  if (world->status != CL_SUCCESS) { return; } 
+  cl_platform_id* platforms = (cl_platform_id *) malloc(num_platforms * sizeof(cl_platform_id));
+  world->status = clGetPlatformIDs(num_platforms, platforms, NULL);
+  if (world->status != CL_SUCCESS) { return; } 
+  for (cl_uint pid = 0; pid < num_platforms; ++pid) {
+    size_t vendor_sz;
+    const cl_platform_id plt = platforms[pid];
+    world->status = clGetPlatformInfo(plt, CL_PLATFORM_VENDOR, 0, NULL, &vendor_sz);
+    if (world->status != CL_SUCCESS) { return; } 
+    char * vendor_name = (char *) malloc(vendor_sz * sizeof(char));
+    world->status = clGetPlatformInfo(plt, CL_PLATFORM_VENDOR, vendor_sz, vendor_name, NULL);
+    if (world->status != CL_SUCCESS) { return; } 
+    if (strstr(vendor_name, vendor)) {
+      world->platform = plt;
+      world->vendor_name = vendor_name;
+      break;
+    } else {
+      free(vendor_name);
+    }
+  }
+  if (!world->platform) {
+    world->status = CL_INVALID_PLATFORM;
+  }
+  free(platforms);
+}
+
+void PopulateDevice(char *device, struct XCLWorld* world) {
+  cl_uint num_devices;
+  world->device = 0;
+  world->status = clGetDeviceIDs(world->platform, CL_DEVICE_TYPE_ACCELERATOR, 0, NULL, &num_devices);
+  if (world->status != CL_SUCCESS) { return; }
+  cl_device_id* devices = (cl_device_id *) malloc(num_devices * sizeof(cl_device_id));
+  world->status = clGetDeviceIDs(world->platform, CL_DEVICE_TYPE_ACCELERATOR, num_devices, devices, NULL);
+  if (world->status != CL_SUCCESS) { return; }
+  for (cl_uint did = 0; did < num_devices; ++did) {
+    size_t name_sz;
+    const cl_device_id dev = devices[did];
+    world->status = clGetDeviceInfo(dev, CL_DEVICE_NAME, 0, NULL, &name_sz);
+    if (world->status != CL_SUCCESS) { return; }
+    char * device_name = (char *) malloc(name_sz * sizeof(char));
+    world->status = clGetDeviceInfo(dev, CL_DEVICE_NAME, name_sz, device_name, NULL);
+    if (world->status != CL_SUCCESS) { return; } 
+    if (strstr(device_name, device)) {
+      world->device = dev;
+      world->device_name = device_name;
+      break;
+    } else {
+      free(device_name);
+    }
+  }
+  free(devices);
+  if (!device) {
+    world->status = CL_INVALID_DEVICE;
+  }
+}
+
+void PopulateContext(struct XCLWorld* world) {
+  world->context = clCreateContext(0, 1, &world->device, NULL, NULL, &world->status);
+}
+
+void PopulateQueue(struct XCLWorld* world) {
+  world->queue = clCreateCommandQueue(world->context, world->device, CL_QUEUE_PROFILING_ENABLE, &world->status);
+}
+
+void PopulateProgram(char *binary, struct XCLWorld* world) {
+  world->binary_name = strdup(binary);
+  size_t fsize = LoadFile(binary, NULL);
+  if (fsize == 0) {
+    fprintf(stderr, "Failed to open file %s\n", binary);
+    exit(EXIT_FAILURE);
+  }
+  world->binary_data = (unsigned char *) malloc(fsize + 1);
+  if (LoadFile(binary, world->binary_data) != fsize) {
+    fprintf(stderr, "Failed to read file %s\n", binary);
+    free(world->binary_data);
+    exit(EXIT_FAILURE);
+  }
+  const unsigned char *program_start = (const unsigned char *) world->binary_data;
+  world->program = clCreateProgramWithBinary(world->context, 1, &world->device, &fsize, &program_start, NULL, &world->status);
+  if (world->status != CL_SUCCESS) { return; } 
+  if (!world->program) {
+    world->status = CL_BUILD_PROGRAM_FAILURE;
+  }
+}
+
+size_t LoadFile(char *binary, unsigned char *data) {
+  FILE *f = fopen(binary, "rb");
+  if (!f) { return 0; }
+  fseek(f, 0, SEEK_END);
+  size_t size = ftell(f);
+  if (data) {
+    fseek(f, 0, SEEK_SET);
+    if (size != fread(data, sizeof(unsigned char), size, f)) {
+      fclose(f);
+      return 0;
+    }
+    fclose(f);
+    data[size] = '\0';
+  }
+  return size;
+}
